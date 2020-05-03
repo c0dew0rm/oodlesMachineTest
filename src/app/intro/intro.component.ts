a0ea7c6d0c26c31c0fcaf556ef29450296736703
rahul110 <rahul@transpacks.co>
2020-05-04 00:33:09 +0530

    Setting up structure

---
+++ src/app/intro/intro.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.css']
+})
+export class IntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getUrl() {
+    return '../../assets/images/backgrounds/Rectangle11.png'
+  }
+
+}
