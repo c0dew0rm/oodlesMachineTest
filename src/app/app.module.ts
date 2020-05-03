b3761d89985bda6dfddda973b954fc3ee66e23a1
rahul110 <rahul@transpacks.co>
2020-05-04 01:00:29 +0530

    Adding header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
+import { HeaderComponent } from './intro/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroComponent
+    IntroComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
