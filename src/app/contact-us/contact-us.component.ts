0381dbf96455946f5e33f8726728fe7417fc04ad
rahul110 <rahul@transpacks.co>
2020-05-05 18:40:50 +0530

    Adding form validations

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+  myForm: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.myForm = new FormGroup({
+      name: new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(32), Validators.pattern('[aA-zZ]*')]),
+      email: new FormControl('',[Validators.required, Validators.pattern("^([aA-zZ0-9._%+-]+)(@+)([aA0-zZ9]+)(.+)([a-z]{2,4})$")]),
+      company: new FormControl('',[Validators.required, Validators.pattern('[aA0-zZ9]*')]),
+      subject: new FormControl('',[Validators.required, Validators.maxLength(15)]),
+      message: new FormControl('',[Validators.required, Validators.minLength(30), Validators.maxLength(150)])
+    });
+  }
+
+  onSubmit(form: FormGroup) {
+    console.log(this.myForm.value);
+    alert("Contact form has been submitted successfully.")
   }
 
 }
